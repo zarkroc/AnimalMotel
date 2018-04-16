1ab14f1b9be19efa55f7c5066d2b5f2b40acae39
zarkroc <tomas.perers@gmail.com>
2018-04-16 10:00:14 +0200

    Assignment 3

---
+++ AnimalMotel/FoodForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AnimalMotel
+{
+    /// <summary>
+    /// Interaction logic for FoodForm.xaml
+    /// </summary>
+    public partial class FoodForm : Window
+    {
+        public FoodForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
